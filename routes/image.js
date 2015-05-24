362074b8497d8a083e13edcb3ced5092aa4cf04b
Andrey Kazakov <a.v.kazakou@gmail.com>
2015-05-24 10:39:08 +0300

    Remove AJAX request for lifeup and delete image

--- routes/image.js
+++ routes/image.js
@@ -3,9 +3,7 @@ var config = require('config');
 module.exports.autoroute = {
     get: {
         '/image/:hash': imageAction,
-        '/lifeup/:hash': lifeupAction
-    },
-    delete: {
+        '/lifeup/:hash': lifeupAction,
         '/delete/:hash': deleteAction
     }
 };
@@ -37,7 +35,7 @@ function deleteAction(req, res) {
         var fs = require('fs-extra');
         var path = config.get('upload.directory') + hash + '.png';
         fs.delete(path, function () {
-            res.send("");
+            res.redirect('/');
         });
     };
 
@@ -49,6 +47,6 @@ function deleteAction(req, res) {
 function lifeupAction(req, res) {
     var hash = req.params.hash;
     serviceLoader.get('ManagerImages').lifeup(hash, function (result) {
-        res.send();
+        res.redirect(config.get('upload.url') + hash);
     });
 }
